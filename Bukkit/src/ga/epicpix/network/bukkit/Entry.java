5f695ba467b5e7e418c49d0812a63750e840f9d2
EpicPix <epicpixminecraft@gmail.com>
2021-07-01 14:52:38 +0200

    Set up stuff

---
+++ Bukkit/src/ga/epicpix/network/bukkit/Entry.java
@@ -0,0 +1,4 @@
+package ga.epicpix.network.bukkit;
+
+public class Entry extends {
+}
---
+++ BungeeCord/src/ga/epicpix/network/bungee/Entry.java
@@ -0,0 +1,7 @@
+package ga.epicpix.network.bungee;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public class Entry extends Plugin {
+
+}

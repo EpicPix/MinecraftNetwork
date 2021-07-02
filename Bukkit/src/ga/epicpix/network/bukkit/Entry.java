2f10bfecbff70efd4a6b3c77bb1e6dc9715145d9
EpicPix <epicpixminecraft@gmail.com>
2021-07-02 13:37:01 +0200

    Fix build error in bukkit version

--- Bukkit/src/ga/epicpix/network/bukkit/Entry.java
+++ Bukkit/src/ga/epicpix/network/bukkit/Entry.java
@@ -5,7 +5,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class Entry extends JavaPlugin {
 
     public void onLoad() {
-        System.out.println("Server Info: " + ServerInfo.getThisServer());
+        System.out.println("Server Info: " + BukkitCommon.getThisServer());
     }
 
 }

f81112b29dbbec648bebf4207939b17e6125e994
EpicPix <epicpixminecraft@gmail.com>
2021-07-04 21:33:22 +0200

    Update ServerInfo

--- Common/src/ga/epicpix/network/common/ServerInfo.java
+++ Common/src/ga/epicpix/network/common/ServerInfo.java
@@ -1,13 +1,52 @@
 package ga.epicpix.network.common;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class ServerInfo {
 
-    public enum ServerType {
-        UNKNOWN
+    public static class ServerType {
+        public static final ServerType UNKNOWN = new ServerType("UNKNOWN");
+
+        private static final ArrayList<ServerType> types = new ArrayList<>();
+
+        private final String id;
+
+        public ServerType(String id) {
+            this.id = id;
+            types.add(this);
+        }
+
+        public final String getId() {
+            return id;
+        }
+
+        public final String toString() {
+            return getId();
+        }
+
+        public static List<ServerType> getTypes() {
+            return Collections.unmodifiableList(types);
+        }
+
+        public static ServerType getType(String id) {
+            return getType(id, false);
+        }
+
+        public static ServerType getType(String id, boolean ignoreCase) {
+            for(ServerType type : types) {
+                if((ignoreCase && type.getId().equalsIgnoreCase(id)) || (!ignoreCase && type.getId().equals(id))) {
+                    return type;
+                }
+            }
+            return null;
+        }
+
     }
 
     public String id;
-    public ServerType type;
+    public ServerType type = ServerType.UNKNOWN;
     public int maxPlayers;
 
     public String toString() {

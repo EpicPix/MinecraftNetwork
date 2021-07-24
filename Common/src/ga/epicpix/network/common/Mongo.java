d2774421e1374636789ecdec786ecbbd03af5ce9
EpicPix <epicpixminecraft@gmail.com>
2021-07-25 01:51:27 +0200

    Removed MongoWatcher

--- Common/src/ga/epicpix/network/common/Mongo.java
+++ Common/src/ga/epicpix/network/common/Mongo.java
@@ -10,8 +10,6 @@ public class Mongo {
 
     private static MongoClient client;
 
-    public static void registerWatcher(MongoWatcher watcher) {}
-
     public static MongoCollection<Document> getCollection(String database, String collection) {
         if(client==null) client = MongoClients.create(MongoCredentials.get().toConnectionString());
         return client.getDatabase(database).getCollection(collection);

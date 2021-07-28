67c2ebf08ed03f559c833fe77b72989597250f69
EpicPix <epicpixminecraft@gmail.com>
2021-07-28 22:08:02 +0200

    Update java secrets.json to secrets.properties

--- Common/src/ga/epicpix/network/common/Secrets.java
+++ Common/src/ga/epicpix/network/common/Secrets.java
@@ -1,42 +1,37 @@
 package ga.epicpix.network.common;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-
-import java.io.ByteArrayOutputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
+import java.util.Properties;
 
 public class Secrets {
 
-    private static JsonObject secrets;
+    private static Properties secrets;
 
     private static void loadSecrets() {
         try {
-            InputStream in = Secrets.class.getResourceAsStream("/secrets.json");
+            InputStream in = Secrets.class.getResourceAsStream("/secrets.properties");
             if(in==null) {
-                throw new FileNotFoundException("Secrets file not found! (secrsts.json)");
+                throw new FileNotFoundException("Secrets file not found! (secrsts.properties)");
             }
-            ByteArrayOutputStream result = new ByteArrayOutputStream();
-            byte[] buffer = new byte[1024];
-            for (int length; (length = in.read(buffer)) != -1; ) {
-                result.write(buffer, 0, length);
+            if(secrets==null) {
+                secrets = new Properties();
+            }else {
+                secrets.clear();
             }
-            secrets = new Gson().fromJson(result.toString(StandardCharsets.UTF_8), JsonObject.class);
+            secrets.load(in);
         }catch(IOException e) {
             e.printStackTrace();
         }
     }
 
-    public static JsonElement getSecret(String secret) {
+    public static String getSecret(String secret) {
         if(secrets==null) loadSecrets();
-        if(secret.equalsIgnoreCase("serverController") && !Reflection.getCaller().equals("ga.epicpix.network.common.websocket.WebSocketCredentials")) {
-            throw new IllegalCallerException("Tried to access Server Controller Login Credentials secrets!");
+        if(secret.startsWith("serverController.") && !Reflection.getCaller().equals("ga.epicpix.network.common.websocket.WebSocketCredentials")) {
+            throw new IllegalCallerException("Tried to access Server Controller secrets!");
         }
-        return secrets.get(secret);
+        return (String) secrets.get(secret);
     }
 
 }
--- Common/src/ga/epicpix/network/common/websocket/WebSocketCredentials.java
+++ Common/src/ga/epicpix/network/common/websocket/WebSocketCredentials.java
@@ -1,24 +1,24 @@
 package ga.epicpix.network.common.websocket;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonObject;
 import ga.epicpix.network.common.Secrets;
 
 import java.net.URI;
 
-record WebSocketCredentials(boolean secure, String host, int port, String username, String password) {
+record WebSocketCredentials(String url, String username, String password) {
 
     static WebSocketCredentials set(WebSocketCredentials existing, String username, String password) {
-        return new WebSocketCredentials(existing.secure, existing.host, existing.port, username, password);
+        return new WebSocketCredentials(existing.url, username, password);
     }
 
     static WebSocketCredentials get() {
-        JsonObject obj = new Gson().fromJson(Secrets.getSecret("serverController"), JsonObject.class);
-        return new WebSocketCredentials(obj.get("secure").getAsBoolean(), obj.get("host").getAsString(), obj.get("port").getAsInt(), obj.get("username").getAsString(), obj.get("password").getAsString());
+        String url = Secrets.getSecret("serverController.url");
+        String username = Secrets.getSecret("serverController.username");
+        String password = Secrets.getSecret("serverController.password");
+        return new WebSocketCredentials(url, username, password);
     }
 
     URI toURI() {
-        return URI.create((secure?"wss":"ws") + "://" + host + ":" + port);
+        return URI.create(url);
     }
 
 }

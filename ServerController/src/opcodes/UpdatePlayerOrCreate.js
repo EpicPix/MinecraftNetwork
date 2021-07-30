386347e94186ec3e9e586cdcb5d81f69fe5f00a9
EpicPix <epicpixminecraft@gmail.com>
2021-07-30 19:25:34 +0200

    Fix bug where authentication wasn't checked

--- ServerController/src/opcodes/UpdatePlayerOrCreate.js
+++ ServerController/src/opcodes/UpdatePlayerOrCreate.js
@@ -1,19 +1,21 @@
 const { ErrorNumbers } = require('../opcodes');
 
 module.exports = function(websocket, json) {
-    if(json['data']) {
-        if(json['uuid']) {
-            if(json['username']) {
-                var player = require('../index').getPlayerOrCreate({uuid: json['uuid'], username: json['username'], rank: require('../index').getDefaultRank().id, firstLogin: Date.now(), lastLogin: -1});
-                require('../index').updatePlayer(player, json['data']);
-                websocket.respond(json, {ok: true, player});
+    if(websocket.checkAuth()) {
+        if(json['data']) {
+            if(json['uuid']) {
+                if(json['username']) {
+                    var player = require('../index').getPlayerOrCreate({uuid: json['uuid'], username: json['username'], rank: require('../index').getDefaultRank().id, firstLogin: Date.now(), lastLogin: -1});
+                    require('../index').updatePlayer(player, json['data']);
+                    websocket.respond(json, {ok: true, player});
+                }else {
+                    websocket.respond(json, {ok: false, errno: ErrorNumbers.NO_USERNAME_FIELD});
+                }
             }else {
-                websocket.respond(json, {ok: false, errno: ErrorNumbers.NO_USERNAME_FIELD});
+                websocket.respond(json, {ok: false, errno: ErrorNumbers.NO_UUID_FIELD});
             }
         }else {
-            websocket.respond(json, {ok: false, errno: ErrorNumbers.NO_UUID_FIELD});
+            websocket.respond(json, {ok: false, errno: ErrorNumbers.NO_DATA_FIELD});
         }
-    }else {
-        websocket.respond(json, {ok: false, errno: ErrorNumbers.NO_DATA_FIELD});
     }
 }

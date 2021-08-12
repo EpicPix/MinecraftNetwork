da47ba7bed44e2cd95e072aafb4f8edaa213767e
EpicPix <epicpixminecraft@gmail.com>
2021-08-12 04:26:01 +0200

    Fix test error

--- Common/src/ga/epicpix/network/common/net/http/HttpRequester.java
+++ Common/src/ga/epicpix/network/common/net/http/HttpRequester.java
@@ -5,7 +5,7 @@ import ga.epicpix.network.common.net.Requester;
 
 public class HttpRequester implements Requester {
 
-    public void sendRequest(Requesta req) {
+    public void sendRequest(Request req) {
         if(!(req instanceof HttpRequest)) {
             throw new IllegalArgumentException("Request is not HttpRequest!");
         }

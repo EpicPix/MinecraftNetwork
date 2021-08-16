6e74f64260c130a7b2d269eb1cded86d336a6e95
EpicPix <48453873+EpicPix@users.noreply.github.com>
2021-08-16 19:11:48 +0200

    Bukkit modules (#5)
    * Create example modules
    * Add requests and make modules in Common
    * Add module requests in node

---
+++ Common/src/ga/epicpix/network/common/modules/Module.java
@@ -0,0 +1,8 @@
+package ga.epicpix.network.common.modules;
+
+public class Module {
+
+    public void enable() {}
+    public void disable() {}
+
+}
---
+++ modules/example/Entry.java
@@ -0,0 +1,10 @@
+package example;
+
+import ga.epicpix.network.common.modules.Module;
+
+public class Entry extends Module {
+
+    public void enable() {
+        System.out.println("Test module enabled");
+    }
+}

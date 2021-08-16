7ce18c0d6534c537706c05ba45c94788c7d58d69
EpicPix <epicpixminecraft@gmail.com>
2021-08-16 17:01:44 +0200

    Build images in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,3 +5,24 @@
 # ./builds/Bukkit.jar  - Spigot Plugin
 # ./builds/Bungee.jar  - BungeeCord Plugin
 # ./builds/CLI.jar     - CLI
+
+echo Copying plugins
+
+mkdir -p docker/bukkit/plugins
+cp ./builds/Bukkit.jar ./docker/bukkit/plugins/Plugin.jar
+
+mkdir -p docker/bungee/plugins
+cp ./builds/BungeeCord.jar ./docker/bungee/plugins/Plugin.jar
+
+cd docker
+
+mkdir -p images
+
+cd bukkit
+echo Building bukkit docker
+docker build --no-cache -t net:Bukkit .
+
+cd ../bungee
+
+echo Building bungee docker
+docker build --no-cache -t net:Bungee .
